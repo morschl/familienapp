2877671838d7a775e939f320e5d4ee1c3c59b786
yentar <mehmet.yentar@gmail.com>
2021-09-24 22:16:30 +0200

    weitere Komponente erstellt

--- src/components/Netzwerk.js
+++ src/components/Netzwerk.js
@@ -1,15 +1,48 @@
-import {Button} from 'react-bootstrap'
+import {ButtonToolbar, Button} from 'react-bootstrap'
 import { Component } from 'react';
 
 export default class Netzwerk extends Component {
+
+    constructor(props){
+             super(props);
+             this.state = {
+                 fragen: [
+                 {
+                        frage0: {
+                            ersteller: "Justin",
+                            frage: "oki-doki",
+                        },
+                  },
+                  {
+                    frage0: {
+                             ersteller: "Justin",
+                             frage: "oki-doki",
+                        },
+                  }
+                 ],
+             }
+        }
+
+
+    getErnaehrungFragen(){
+        console.log(this.state.fragen);
+        //console.log(this.state.fragen[1].frage0);
+      }
+
     render() {
         return (
-           <>
-             <Button variant="primary">Ernährung</Button>{' '}
-             <Button variant="secondary">Gesundheit</Button>{' '}
-             <Button variant="success">Spielpartner</Button>{' '}
-             <Button variant="warning">Betreuung</Button>{' '}
-           </>
+        <div class="container h-100">
+            <div class="row align-items-center h-100">
+                <div class="col-6 mx-auto themen">
+                    <ButtonToolbar>
+                        <Button variant="success" onClick={this.getErnaehrungFragen.bind(this)}>Ernährung</Button>
+                        <Button variant="success">Gesundheit</Button>
+                        <Button variant="success">Spielpartner</Button>
+                        <Button variant="success">Betreuung</Button>
+                    </ButtonToolbar>
+               </div>
+            </div>
+        </div>
         )
     }
 }
