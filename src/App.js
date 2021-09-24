5fa809b17c772b9f94d707629d6500da5f4f4df1
yentar <mehmet.yentar@gmail.com>
2021-09-24 18:04:31 +0200

    component Folder und eine Test-Componente erstellt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
 
     <div className="App">
       <header className="App-header">
-
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-light">Light</button>
-    <button type="button" class="btn btn-dark">Dark</button>
-
-<button type="button" class="btn btn-link">Link</button>
+      <AppHeader />
        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/AppHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class AppHeader extends Component {
+    render() {
+        return(
+        <div>
+            <p>Hier steht eine App</p>
+        </div>
+
+        )
+      }
+}
