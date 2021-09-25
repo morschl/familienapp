038ab6730c9c5ed03bcd1e5a3f3c205da8091de9
jds94 <71876112+jds94@users.noreply.github.com>
2021-09-25 14:46:48 +0200

    Logo ausgetauscht

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
-import { Container, Row, Col, Card} from 'react-bootstrap'
-import React, { Component } from 'react';
+import {Container, Row, Col, Card} from 'react-bootstrap'
+import React, {Component} from 'react';
 
 import './Dashboard.css'
 import Netzwerk from './Netzwerk';
@@ -23,9 +23,9 @@ export default class Dashboard extends Component {
 
     render() {
         if (this.state.text === "nestwerken") {
-            return (<Netzwerk />)
+            return (<Netzwerk/>)
         } else if (this.state.text === "veranstaltungen") {
-            return (<Veranstaltungen />)
+            return (<Veranstaltungen/>)
         } else if (this.state.text === "leihboerse") {
             return (<Leihboerse/>)
         } else if (this.state.text === "spielplatztalk") {
@@ -35,36 +35,40 @@ export default class Dashboard extends Component {
                 <Container className='container-fluid'>
                     <Row className="justify-content-md-center">
                         <Col xs={2}>
-                            <Card style={{ border: 'none' }}>
-                                <Card.Img variant="top" src="./logo.svg" onClick={this.getThemen.bind(this, "nestwerken")} />
+                            <Card style={{border: 'none'}}>
+                                <Card.Img variant="top" src="./nest.svg"
+                                          onClick={this.getThemen.bind(this, "nestwerken")}/>
                                 <Card.Body>
-                                    <Card.Title style={{ textAlign: 'center' }}>Nestwerken</Card.Title>
+                                    <Card.Title style={{textAlign: 'center'}}>Nestwerken</Card.Title>
                                 </Card.Body>
                             </Card>
                         </Col>
                         <Col xs={2}>
-                            <Card style={{ border: 'none' }}>
-                                <Card.Img variant="top" src="./kalender.svg" onClick={this.getThemen.bind(this, "veranstaltungen")} />
+                            <Card style={{border: 'none'}}>
+                                <Card.Img variant="top" src="./kalender.svg"
+                                          onClick={this.getThemen.bind(this, "veranstaltungen")}/>
                                 <Card.Body>
-                                    <Card.Title style={{ textAlign: 'center' }}>Kurse & Veranstaltungen</Card.Title>
+                                    <Card.Title style={{textAlign: 'center'}}>Kurse & Veranstaltungen</Card.Title>
                                 </Card.Body>
                             </Card>
                         </Col>
                     </Row>
                     <Row className="justify-content-md-center">
                         <Col xs={2}>
-                            <Card style={{ border: 'none' }}>
-                                <Card.Img variant="top" src="./tauschboerse.svg" onClick={this.getThemen.bind(this, "leihboerse")} />
+                            <Card style={{border: 'none'}}>
+                                <Card.Img variant="top" src="./tauschboerse.svg"
+                                          onClick={this.getThemen.bind(this, "leihboerse")}/>
                                 <Card.Body>
-                                    <Card.Title style={{ textAlign: 'center' }}>Tauschen & Leihen</Card.Title>
+                                    <Card.Title style={{textAlign: 'center'}}>Tauschen & Leihen</Card.Title>
                                 </Card.Body>
                             </Card>
                         </Col>
                         <Col xs={2}>
-                            <Card style={{ border: 'none' }}>
-                                <Card.Img variant="top" src="./spielplatz.svg" onClick={this.getThemen.bind(this, "spielplatztalk")} />
+                            <Card style={{border: 'none'}}>
+                                <Card.Img variant="top" src="./spielplatz.svg"
+                                          onClick={this.getThemen.bind(this, "spielplatztalk")}/>
                                 <Card.Body>
-                                    <Card.Title style={{ textAlign: 'center' }}>Spielplatz-Talk</Card.Title>
+                                    <Card.Title style={{textAlign: 'center'}}>Spielplatz-Talk</Card.Title>
                                 </Card.Body>
                             </Card>
                         </Col>
--- src/components/SpielplatzTalk.js
+++ src/components/SpielplatzTalk.js
@@ -6,7 +6,7 @@ export default class SpielplatzTalk extends Component {
 
     render() {
         return (
-            <iframe src="https://ufopixel.de/spielplatz/" width="100%" height="2000" name="SELFHTML_in_a_box">
+            <iframe title='Spielplatz' src="https://ufopixel.de/spielplatz/" width="100%" height="800" name="SELFHTML_in_a_box">
   <p>Ihr Browser kann leider keine eingebetteten Frames anzeigen:
   Sie können die eingebettete Seite über den folgenden Verweis aufrufen: 
   <a href="https://ufopixel.de/spielplatz/">SpielplatzTalk</a>
