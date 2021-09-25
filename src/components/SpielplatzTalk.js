2d1686debb56404be2509d652d4fecb168c07c97
Frank Mueller <frmimue@gmail.com>
2021-09-25 04:09:03 -0700

    a

--- src/components/SpielplatzTalk.js
+++ src/components/SpielplatzTalk.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ReactDOM } from 'react-dom';
 
 import './SpielplatzTalk.css'
 
@@ -10,10 +11,22 @@ export default class SpielplatzTalk extends Component {
             text: ""
         };
     }
+    
+
+    componentDidMount() {
+        let canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
+        let ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'rgb(200,0,0)';
+        ctx.fillRect(10, 10, 55, 50);
+    }
 
     render() {
         return (
-            <div>Spielplatz-Talk</div>
+            <div>
+                <canvas ref="myCanvas" width="400" height="400">
+                </canvas>
+                <img src="./maps.png" /></div>
         )
     }
 }
