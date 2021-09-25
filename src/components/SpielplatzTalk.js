ca51ec564975da0f9e026a839090cdaa633e4a78
Frank Mueller <frmimue@gmail.com>
2021-09-25 05:13:57 -0700

    SpielplatzTalk

--- src/components/SpielplatzTalk.js
+++ src/components/SpielplatzTalk.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react';
-import { ReactDOM } from 'react-dom';
 
 import './SpielplatzTalk.css'
 
 export default class SpielplatzTalk extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: ""
-        };
-    }
-    
-
-    componentDidMount() {
-        let canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
-        let ctx = canvas.getContext('2d');
-
-        ctx.fillStyle = 'rgb(200,0,0)';
-        ctx.fillRect(10, 10, 55, 50);
-    }
-
     render() {
         return (
-            <div>
-                <canvas ref="myCanvas" width="400" height="400">
-                </canvas>
-                <img src="./maps.png" /></div>
+            <iframe src="https://ufopixel.de/spielplatz/" width="100%" height="2000" name="SELFHTML_in_a_box">
+  <p>Ihr Browser kann leider keine eingebetteten Frames anzeigen:
+  Sie können die eingebettete Seite über den folgenden Verweis aufrufen: 
+  <a href="https://ufopixel.de/spielplatz/">SpielplatzTalk</a>
+  </p>
+</iframe>
         )
     }
 }
